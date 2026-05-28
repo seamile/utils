fb98f93b216c5af05062a3b38d3937a7b1146ebf
Seamile <lanhuermao@gmail.com>
2026-05-28 18:46:13 +0800

    feat: 优化 tunnel.sh 和 ssh-tunnel.service.plist，修复 SSH 代理逻辑，增强日志记录和连接管理

--- tunnel.sh
+++ tunnel.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 PIPE="/tmp/ssh-tunnel.pipe"
-PROXY_BIND="127.0.0.1"
+PROXY_HOST="127.0.0.1"
 PROXY_PORT=1086
+SSH_PATTERN="^ssh .*-D ${PROXY_HOST}:${PROXY_PORT}"  # 用户匹配所有绑定到指定 IP 和端口的 ssh 进程
 
 # 记录日志
 function log() {
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $*";
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $*"
 }
 
 # 打开 SSH 代理
@@ -16,42 +17,30 @@ function open_tunnel() {
         return 0
     else
         log "Opening tunnel to '$1'"
-        ssh -f -qTnN -D "${PROXY_BIND}:${PROXY_PORT}" "$1"  # 通过 -f 在后台运行
+        ssh -f -qTnN -D "${PROXY_HOST}:${PROXY_PORT}" "$1"
         log "Tunnel opened"
-    fi
-}
-
-# 获取进程 ID
-function get_tunnel_pid() {
-    pid=$(pgrep -f "^ssh .*-D ${PROXY_BIND}:${PROXY_PORT}")
-    if [ -n "$pid" ]; then
-        echo $pid
-    else
-        return 1
     fi
 }
 
 # 检查 SSH 代理是否打开
 function is_tunnel_opened() {
-    pkill -0 -f "^ssh .*-D ${PROXY_BIND}:${PROXY_PORT}"
+    # 不使用 pid 文件，是为了防止在异常退出后，pid 文件未被删除导致的误判
+    pkill -0 -f "$SSH_PATTERN"
 }
 
 # 关闭 SSH 代理
 function close_tunnel() {
     log 'Closing tunnel'
-    pkill -f "^ssh .*-D ${PROXY_BIND}:${PROXY_PORT}"
+    pkill -f "$SSH_PATTERN"  # kill 掉所有匹配的 ssh 进程
+    while is_tunnel_opened; do
+        sleep 0.5  # 等待进程完全退出
+    done
 }
 
 # 检查守护进程 (tunnel.sh) 是否运行
 function is_daemon_running() {
-    pkill -0 -f "$(basename $0)" && [ -p "$PIPE" ]
-}
-
-# 关闭守护进程
-function kill_daemon() {
-    log "Killing daemon"
-    rm -f "$PIPE"
-    pkill -f "$(basename $0)"
+    local pids=$(pgrep -f "^/[a-z/]+/(sh|bash|zsh|fish) .*$(basename "$0")" | grep -v "^$$\$")
+    [ -n "$pids" ] && [ -p "$PIPE" ]
 }
 
 # 确保 "命名管道" 存在
@@ -62,30 +51,39 @@ function touch_pipe() {
     fi
 }
 
-# 监听管道，处理新的连接请求
-function listen_pipe() {
-    touch_pipe $PIPE
+# 清理管道文件
+function cleanup() {
+    log "Cleaning up"
+    rm -f "$PIPE"
+}
 
-    while [ -p "$PIPE" ]; do
-        if read server < "$PIPE"; then
+# 监听管道，处理新的连接请求
+function tunnel_daemon() {
+    log "Starting daemon"
+    current_server=$1
+    touch_pipe "$PIPE"
+    exec 3<>"$PIPE"
+    trap cleanup EXIT
+
+    while true; do
+        if ! is_tunnel_opened; then
+            open_tunnel "$current_server"
+            sleep 1
+        fi
+        if read -t 1 -u 3 current_server; then
             # 先关闭当前隧道
             close_tunnel
 
             # 如果输入是 exit，则退出
-            if [[ "$server" == "exit" ]]; then
+            if [[ "$current_server" == "exit" ]]; then
                 log "Bye"
                 return 0
             fi
 
             # 重新连接到指定服务器
-            open_tunnel $server
-        else
-            # 理论上不会发生，但为安全起见，加一个短暂的等待
-            sleep 0.3
+            open_tunnel "$current_server"
         fi
     done
-
-    rm -f "$PIPE"
 }
 
 function usage() {
@@ -98,10 +96,8 @@ if [[ $# -eq 0 ]]; then
     usage
     exit 1
 elif is_daemon_running; then
-    echo "$1" > $PIPE
+    echo "$1" > "$PIPE"
 else
-    kill_daemon
     close_tunnel
-    open_tunnel $1
-    listen_pipe
+    tunnel_daemon "$1"
 fi
